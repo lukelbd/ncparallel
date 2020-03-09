92891339c2abd3c715431757a40ed762b2d8efbb
Luke Davis <lukelbd@gmail.com>
2020-03-09 14:13:32 -0600

    Header cleanup

--- header.sh
+++ header.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
-################################################################################
-# Helper functions
-################################################################################
-# Function prints message to standard error and exits main process
+# Helper functions sourced by main scripts
+# Print message to standard error and exit main process
 raise() {
   echo "Usage: $usage"
   echo "Error: $1" 1>&2
   exit 1
 }
-# Function that waits for background processes to finish, and makes
-# sure they were all successful
+
+# Wait for background processes to finish and make sure they were successful
 pwait() {
   local cmd pids
   cmd="$1"
   pids=("${@:2}")
   for pid in "${pids[@]}"; do
-    wait $pid
-    [ $? -ne 0 ] && raise "At least one of the '$cmd' processes failed."
+    wait $pid || raise "At least one of the '$cmd' processes failed."
   done
 }
 
