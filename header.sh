8473f9d893db3b6c15c6b3f7d10ea7b0933a6ca0
Luke Davis <lukelbd@gmail.com>
2020-03-11 15:07:32 -0600

    Echo error to stderr

--- header.sh
+++ header.sh
@@ -2,7 +2,7 @@
 # Helper functions sourced by main scripts
 # Print message to standard error and exit main process
 raise() {
-  echo "Usage: $usage"
+  echo "Usage: $usage" 1>&2
   echo "Error: $*" 1>&2
   exit 1
 }
